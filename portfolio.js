8ce622ab9917931fba63304543bd3e9eb6e03966
SHUBHAM4830 <shubhamjha4830@gmail.com>
2024-08-16 23:34:38 +0530

    Update portfolio.js

--- portfolio.js
+++ portfolio.js
@@ -6,3 +6,15 @@ function hideSidebar(){
     const sidebar = document.querySelector('.sidebar')
     sidebar.style.display = 'none'
 }
+let btn = document.getElementById('btn');
+btn.addEventListener("click", function() {
+  btn.setAttribute('class', 'submit process');
+  btn.innerHTML = 'Processing';
+  setTimeout(()=>{
+     btn.setAttribute('class', 'submit submitted');
+     btn.innerHTML = `
+     <span class="tick">&#10004;</span>
+     Submitted
+     `;
+  },5000);
+});
